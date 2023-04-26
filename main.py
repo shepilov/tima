31dccaaa2afe2c26d77337fdcf732418d8f2a4bb
timaterror <86703974+timaterror@users.noreply.github.com>
2023-04-26 13:09:04 +0000

    нахождение корней в уравнениях

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-for _ in range(100):
-    print('hello')
+import sympy
+from sympy import symbols
+from sympy.solvers import solve
+#задаем сивол который будет переменной в уравнении
+x = symbols('x')
 
+#уравнение равное нулю
+eq = input('enter equation: 0 = ')
+#решение уравнения
+solution = solve(eq, x)
+#выводит все корни по очереди
+for s in solution:
+    print("x = ", s)
+#factorise
+print(sympy.factor(eq))
